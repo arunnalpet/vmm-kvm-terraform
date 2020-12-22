a67fa3cd65545ee26d7941c95c13d8dad92c30c3
arunnalpet <arunnalpet@gmail.com>
2020-12-22 10:08:24 +0000

    Adding image download script

---
+++ scripts/download_cloud_images.sh
@@ -0,0 +1,2 @@
+wget https://cloud-images.ubuntu.com/releases/xenial/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img
+wget https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
